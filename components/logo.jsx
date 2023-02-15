b35985c69ee86d9c9488b783c87ab8ef6bce5341
davidtaim <davidtaim@hotmail.com>
2023-02-14 22:29:00 -0600

    first commit

---
+++ components/logo.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link"
+import FootprintIcon from "./icons/footprint"
+import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from "@emotion/styled"
+
+const LogoBox = styled.span`
+font-weight: bold;
+font-size: 18px;
+display: inline-flex;
+align-items: center;
+height: 30px;
+line-height: 20px;
+padding: 10px;
+
+&:hover img {
+    transform: rotate(20deg);
+}
+`
+
+const Logo = () => {
+    return (
+        <Link href="/" scroll={false}>
+            <LogoBox>
+                <FootprintIcon />
+                <Text color={useColorModeValue('gray.800', 'whiteAlpha.900')} 
+                fontFamily='M PLUS Rounded 1c'
+                fontWeight="bold"
+                ml={3}>
+                    David Chavez Rodriguez
+                </Text>
+            </LogoBox>
+        </Link>
+    )
+
+}
+
+export default Logo
---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius={"lg"} bg={"red"} p={3} mb={6} textAlign="center">
+                Hello, I&apos;m a full-stack developer based in Mexico!
+            </Box>
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        David Chavez Rodriguez
+                    </Heading>
+                    <p>davidtaim (Developer)</p>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default Page
